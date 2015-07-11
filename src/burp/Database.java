1eb44968400878c64472d536d6dfb76d0d2fb23b
Scott Johnson <scott.johnson@gmail.com>
2015-07-11 08:43:00 -0500

    update Database.upsert() with getConnection() code

--- src/burp/Database.java
+++ src/burp/Database.java
@@ -125,20 +125,12 @@ public class Database {
 	/**
 	 * TODO: add methods for storing/retrieving data
 	 */
-	
 	public boolean upsert(Parameter toUpsert) {
-		try {
-			//don't know that we need this, but couldn't hurt
-			if (this.conn == null) {
-				this.conn = DriverManager
-						.getConnection("jdbc:sqlite:burp-hash.db");
-			}
-			//Want to update if exists, update if not
-			//INSERT OR REPLACE INTO table(name, hash) VALUES (toUpsert.name, toUpsert.hash);
-			return true;
-		} catch (SQLException e) {
-			this.stdErr.println(e.getMessage());
-			return false;
+		if (this.conn == null) {
+			this.conn = this.getConnection();
 		}
+		//Want to update if exists, update if not
+		//INSERT OR REPLACE INTO table(name, hash) VALUES (toUpsert.name, toUpsert.hash);
+		return true;
 	}
 }

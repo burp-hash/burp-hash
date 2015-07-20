2d1a834ce0b95d83fe6b0fea9c10bee33d58b362
Scott Johnson <scott.johnson@gmail.com>
2015-07-20 06:49:06 -0500

    cleanup HashAlgorithm

--- src/burp/HashAlgorithm.java
+++ src/burp/HashAlgorithm.java
@@ -1,14 +1,13 @@
 package burp;
 
 import java.util.regex.Pattern;
-import java.util.regex.Matcher;
 
 public class HashAlgorithm
 {
 	public int charWidth;
 	public HashAlgorithmName name;
 	public Pattern pattern;
-	private static final String hexRegex = "([a-fA-F0-9]{%s})";
+	private final String hexRegex = "([a-fA-F0-9]{%s})";
 	
 	public HashAlgorithm(int charWidth, HashAlgorithmName name)
 	{
